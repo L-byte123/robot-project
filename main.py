15503c8ed519c3e3b6ce1919dd697649de39060f
L-byte123 <3640872884@qq.com>
2026-08-26 20:03:36 +0800

    接入OpenAI API

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-print("机器人启动成功！")
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+
+print("AI 机器人启动成功！")
+
 while True:
-    user_input=input("你：")
-    if user_input=="退出":
+    user_input = input("你：")
+
+    if user_input == "退出":
         print("机器人：再见！")
         break
-    if "你好" in user_input:
-        print("机器人：你好！")
-    elif "名字" in user_input:
-        print("机器人：我叫robot")
-    else:
-        print("机器人：我还不会回答这个问题。")
+
+    response = client.responses.create(
+        model="gpt-5.4-mini",
+        input=user_input
+    )
+
+    print("机器人：", response.output_text)
