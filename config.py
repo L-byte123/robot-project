c1cee8538d27a5e5ff28538f41554f9ba50a6b64
L-byte123 <3640872884@qq.com>
2026-08-27 10:28:23 +0800

    将聊天记录存储迁移到SQLite

--- config.py
+++ config.py
@@ -2,10 +2,9 @@ BOT_NAME = "小智"
 
 MODEL_NAME = "gpt-5.4-mini"
 
-HISTORY_FILE = "chat_history.json"
-
 SYSTEM_PROMPT = """
 你是一个耐心的 Python 学习助手。
 回答尽量清楚。
 遇到代码时要解释每一部分的作用。
 """
+DATABASE_FILE="robot.db"
